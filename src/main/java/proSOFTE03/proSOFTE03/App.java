cca98c73565fdbbb8c5fc537f2537144a94aa5b2
Javier Martín Moreno <javier.martin23@alu.uclm.es>
2017-09-28 10:53:17 +0200

    Add files via upload

---
+++ src/main/java/proSOFTE03/proSOFTE03/App.java
@@ -0,0 +1,13 @@
+package proSOFTE03.proSOFTE03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
